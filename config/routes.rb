90b6ff81930e3b5ab3ee61b3e79bb2eede6badc4
EC2 Default User <ec2-user@ip-172-31-34-217.ap-northeast-1.compute.internal>
2019-10-04 04:51:37 +0000

    投稿画面のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'tweets' => 'tweets#index'
+  get   'tweets/new'  =>  'tweets#new' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
