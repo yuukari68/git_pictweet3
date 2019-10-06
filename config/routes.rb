10fcb0aede5e8a6062eb89f2b5c1f47c76ab0168
EC2 Default User <ec2-user@ip-172-31-34-217.ap-northeast-1.compute.internal>
2019-10-06 14:59:14 +0000

    削除ボタンの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get   'tweets/new'  =>  'tweets#new' 
    post  'tweets'      =>  'tweets#create'
    get   'users/:id'   =>  'users#show' 
+   delete  'tweets/:id'  => 'tweets#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
