8ebc62754e64353e3144fe83a87ca711eaacb38b
EC2 Default User <ec2-user@ip-172-31-34-217.ap-northeast-1.compute.internal>
2019-10-06 13:27:39 +0000

    userコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'tweets' => 'tweets#index'
   get   'tweets/new'  =>  'tweets#new' 
    post  'tweets'      =>  'tweets#create'
+   get   'users/:id'   =>  'users#show' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
