471562e82cfbffa77c79023a6706f0f30f60aa02
EC2 Default User <ec2-user@ip-172-31-34-217.ap-northeast-1.compute.internal>
2019-10-06 12:41:20 +0000

    ページネーションの設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   def index 
-     @tweets =Tweet.all
+     @tweets =Tweet.order("created_at DESC").page(params[:page]).per(5)
    end
      def new
   
