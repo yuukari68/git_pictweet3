676762d2882944fd966f58e35af21d041b571d19
EC2 Default User <ec2-user@ip-172-31-34-217.ap-northeast-1.compute.internal>
2019-10-07 11:26:32 +0000

    コメントフォームのビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
      root  'tweets#index'   
-  get 'tweets' => 'tweets#index'
-  get   'tweets/new'  =>  'tweets#new' 
-   post  'tweets'      =>  'tweets#create'
-   get   'users/:id'   =>  'users#show' 
-   delete  'tweets/:id'  => 'tweets#destroy'
-     get   'tweets/:id/edit'  => 'tweets#edit'
-     patch   'tweets/:id'  => 'tweets#update'
-      get 'tweets/:id' => 'tweets#show'      
+  resources :tweets  do
+        resources :comments, only: [:create]
+  end#tweets_controllerに対してのresourcesメソッド
+  resources :users, only: [:show]      
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
