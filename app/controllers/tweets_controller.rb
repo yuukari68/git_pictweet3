b9944ac586f93a4c3d36fef3fc83bc3c6e868d85
EC2 Default User <ec2-user@ip-172-31-34-217.ap-northeast-1.compute.internal>
2019-10-08 09:57:57 +0000

    ビューとcssの編集

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,7 @@ class TweetsController < ApplicationController
   
    def show
     @tweet = Tweet.find(params[:id])
+     @comments = @tweet.comments.includes(:user)
   end
   
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
   def show
+    user = User.find(params[:id])
     @nickname = current_user.nickname
-    @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
+   @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
   end 
 end
